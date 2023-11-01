c5d36caac1882d62900be9fe2ff13c549e0ba266
fgurgell50 <137560055+fgurgell50@users.noreply.github.com>
2023-11-01 11:22:21 -0300

    commit 31.10

---
+++ frontend/src/utils/config.js
@@ -0,0 +1,37 @@
+export const api = "http://localhost:5000/api"
+export const uploads = "http://localhost:5000/api/uploads"
+
+// metodos da req, dados q serao enviados, token , imagem
+export const requestConfig = ( method, data, token = null, image = null) => {
+
+    let config
+
+    if( image ) {
+        config = {
+            method,
+            body: data,
+            headers: {},
+        }
+    } else if( method === "DELETE" || data === null ) {
+        //like nao vai ter dados
+        config = {
+            method,
+            headers: {},
+        } 
+    }else {
+            config = {
+                method,
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        }
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+
+}
